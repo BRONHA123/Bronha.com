66f3b92a7945a0a3584640423a03c325ed8fb71c
BRONHA123 <55699225+BRONHA123@users.noreply.github.com>
2020-02-16 20:45:04 +0700

    Update Missu.py

--- Missu.py
+++ Missu.py
@@ -1,4 +1,4 @@
-a=raw_input()
-Print(a*1);time.sleep(1)
-Print(a*1);time.sleep(1)
-Print(a*1);time.sleep(1)
+a=raw_input('what you want to say>>> ')
+print(a*1);time.sleep(1)
+print(a*1);time.sleep(1)
+print(a*1);time.sleep(1)
