a8831e4221aa22e6f52bf983c9974638f727e987
BRONHA123 <55699225+BRONHA123@users.noreply.github.com>
2020-02-16 21:00:18 +0700

    Update Missu.py

--- Missu.py
+++ Missu.py
@@ -1,4 +1,8 @@
-a=raw_input('what you want to say>>> ')
+import time
+
+a=raw_input('what you want to say >>> ')
+b=raw_input('what time you want to say >>> ')
+while(c=0,c<b,c++){
 print(a*1)
-print(a*1)
-print(a*1)
+time.sleep(1)
+} 
