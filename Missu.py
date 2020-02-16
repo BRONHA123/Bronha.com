847827fc0c07ac35e6006901b2aa9b02b070f437
BRONHA123 <55699225+BRONHA123@users.noreply.github.com>
2020-02-16 21:51:28 +0700

    Update and rename Missu.py to Robot.py

--- Missu.py
+++
@@ -1,8 +0,0 @@
-import time
-clear()
-
-print(">Hello\n=============================\n1. Hello\n=============================")
-a=raw_input("Choose the option above >>> ")
-while True:
-  print(a)
-  time.sleep(1)
---
+++ Robot.py
@@ -0,0 +1,8 @@
+import time
+clear()
+
+print("[Robot] >>> Hello\n=============================\n1. Hello\n=============================")
+a=raw_input(">Choose the option above\n[You] >>> ")
+print("[Robot] >>> What's your name?")
+b=raw_input()
+print("[Robot] >>> What's your feeling?")
