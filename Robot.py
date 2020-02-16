ba8112f9caf83d80069e9426cd2cfcbe8b7d3210
BRONHA123 <55699225+BRONHA123@users.noreply.github.com>
2020-02-16 22:37:50 +0700

    Update Robot.py

--- Robot.py
+++ Robot.py
@@ -1,20 +1,20 @@
 import time
 
 
-print("[Robot] >>> \n\nHello\n=============================\n1. Hello\n=============================")
+print("\n\n[Robot] >>> Hello\n=============================\n1. Hello\n=============================")
 a=raw_input(">Choose the option above\n[You] >>> ")
 print("[Robot] >>> What's your name?")
 b=raw_input()
 print("[Robot] >>> Nice to meet you "+b) 
-print("[Robot] >>> What's your feeling now?\n=============================\n1. Sad\n=============================\n2. Happy=============================")
+print("[Robot] >>> What's your feeling now?\n=============================\n1. Sad\n2. Happy\n=============================")
 c=raw_input(">Choose the option above\n[You] >>> ")
-if(c==1 or c==sad or c==Sad or c==SAD):
+if(c==1):
   print("[Robot] >>> Be strong, I am always with you!") 
   d=raw_input("[You] >>> ")
   print("[Robot] >>> hmmm i am a baby Robot so i don't have something to say with you more")
   e=raw_input("[You] >>> ")
   print("[Robot] >>> Robot is sleeping") 
-if(c==2 or c==happy or c==Happy or c==HAPPY):
+if(c==2):
   print("[Robot] >>> So good!")
   f=raw_input("[You] >>> ")
   print("[Robot] >>> hmmm i am a baby Robot so i don't have something to say with you more")
