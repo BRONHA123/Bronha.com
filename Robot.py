8c8cf1057a0f2ac951e9b476ec5b93748273574a
BRONHA123 <55699225+BRONHA123@users.noreply.github.com>
2020-02-17 11:19:44 +0700

    Update Robot.py

--- Robot.py
+++ Robot.py
@@ -12,54 +12,16 @@ print(">Choose the option above\n[You] >>> ")
 c=raw_input() 
 
 if(c==1):
+  print("[Robot] >>> Why? ")
+  d=raw_input()
   print("[Robot] >>> Be strong, I am always with you!") 
   d=raw_input("[You] >>> ")
   print("[Robot] >>> Do you love Bronha?\n=============================\n1. Yes!\n2. No! \n=============================\n[You] >>> ")
   h=raw_input() 
   if h==1:
     print("oh.........! Did you tell him already?\n=============================\n1. Yes. \n2. No.\n=============================")
-    i=raw_input()
-    if i==1:
-      Print("[Robot] >>> He love you or not?\n=============================\n1. Yes he love me too.\n2. No! He have his cr.\n=============================")
-      j=raw_input() 
-      if j==1:
-        print("[Robot] >>> wish you and him.....")
-        print("[You] >>> ")
-        print("[Robot] >>> Now i want to sleep") 
-        time.sleep(2)
-        os.exit("[System] >>> Robot is sleeping now!") 
-      
-      if j==2:
-        print("[Robot] >>> Ohh so sad, Be strong, I'm here with you!.")
-        k=raw_input("[You] >>> ") 
-        print("[Robot] >>> I want to sleep now! Goodbye!")
-        time.sleep(2)
-        os.exit("[System] >>> Robot is sleeping now!") 
-      
-    if i==2:
-      print("[Robot] >>> So why you not tell him?")
-      raw_input("[You] >>> ") 
-      print("[Robot] >>> Rrrrrr!")
-      time.sleep(3)
-      print("[Robot] >>> Sorry, I want to sleep now!") 
-      time.sleep(2)
-      os.exit("[System] >>> Robot is sleeping now!") 
     
-  if h==2:
-
-    print("[Robot] >>> why?")
-    raw_input("[You] >>> ") 
-    print("[Robot] >>> Rrrrrr!")
-    time.sleep(3)
-    print("[Robot] >>> Sorry, I want to sleep now!") 
-    time.sleep(2)
-    os.exit("[System] >>> Robot is sleeping now!") 
   
-  print("[Robot] >>> hmmm i am a baby Robot so i don't have something to say with you more")
-  e=raw_input("[You] >>> ")
-  time.sleep(2)
-  print("[System] >>> (Robot is sleeping)") 
-
 if(c==2):
   print("[Robot] >>> So good!")
   f=raw_input("[You] >>> ")
