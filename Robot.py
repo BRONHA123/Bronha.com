10ec31c51c5429f81c2a3180aa3489a6952dad67
BRONHA123 <55699225+BRONHA123@users.noreply.github.com>
2020-02-17 09:35:21 +0700

    Update Robot.py

--- Robot.py
+++ Robot.py
@@ -1,23 +1,23 @@
 import time
 
 
-print("\n\n[Robot] >>> Hello\n=============================\n1. Hello\n=============================")
+print("\n\n[Robot] >>> Hello")
 
 print("[Robot] >>> What's your name?")
-b=raw_input()
+print("[You] >>> "b=raw_input()) 
 print("[Robot] >>> Nice to meet you "+b) 
 print("[Robot] >>> What's your feeling now?\n=============================\n1. Sad\n2. Happy\n=============================")
-print(">Choose the option above\n[You] >>> ")
-c=input()
+print(">Choose the option above\n[You] >>> "c=raw_input()0)
+
 if(c==1):
   print("[Robot] >>> Be strong, I am always with you!") 
   d=raw_input("[You] >>> ")
   print("[Robot] >>> hmmm i am a baby Robot so i don't have something to say with you more")
   e=raw_input("[You] >>> ")
-  print("[Robot] >>> Robot is sleeping") 
+  print("[System] >>> (Robot is sleeping)") 
 if(c==2):
   print("[Robot] >>> So good!")
   f=raw_input("[You] >>> ")
   print("[Robot] >>> hmmm i am a baby Robot so i don't have something to say with you more")
   g=raw_input("[You] >>> ")
-  print("[Robot] >>> Robot is sleeping") 
+  print("[System] >>> (Robot is sleeping)") 
